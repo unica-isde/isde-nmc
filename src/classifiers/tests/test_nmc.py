7a7edee7a094069b4ae4912b38b61edb2bba7e6f
bat <battista.biggio@unica.it>
2024-10-17 09:51:54 +0200

    added NMC skeleton testing class

---
+++ src/classifiers/tests/test_nmc.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestNMC(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_fit(self):
+        pass
+
+    def test_predict(self):
+        pass
+
+
